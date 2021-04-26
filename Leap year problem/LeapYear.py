3b63cdc39026072643a2023176b0864549e5707f
Salman Mumtaz <salmanmumtazmayo@gmail.com>
2021-04-27 00:08:31 +0500

    first commit

---
+++ Leap year problem/LeapYear.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print("leapyear")
+    return leap
+
+year = int(input())
+print(is_leap(year))
