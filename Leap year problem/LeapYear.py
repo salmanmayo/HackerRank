e40405fdc30749b925e53838d2a83b9861c47216
Salman Mumtaz <salmanmumtazmayo@gmail.com>
2021-05-01 02:28:35 +0500

    leap year

--- Leap year problem/LeapYear.py
+++ Leap year problem/LeapYear.py
@@ -1,10 +1,14 @@
 def is_leap(year):
     leap = False
-    if (year % 4) == 0:
-        if (year % 100) == 0:
-            if (year % 400) == 0:
-                print("leapyear")
+    if (year % 400 == 0):
+        return True
+        if (year % 100 == 0):
+            return True
+            if (year % 400 == 0):
+                return False
+    # Write your logic here
+
     return leap
 
+
 year = int(input())
-print(is_leap(year))
